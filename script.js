79d2308d4a26a39c8a04366820927becc00c2c6b
priyalk2023 <153483940+priyalk2023@users.noreply.github.com>
2024-05-03 21:54:17 +0530

    upload file

---
+++ script.js
@@ -0,0 +1,6 @@
+var show = document.getElementById("show");
+var opn = document.getElementById("open");
+opn.addEventListener("click", function () {
+  show.classList.toggle("active");
+});
+// navbar end
